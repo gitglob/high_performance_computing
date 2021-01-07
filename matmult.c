8a945b4588e6177fde36410e06de780453e87d14
Paweł <pawel.darulewski@gmail.com>
2021-01-07 13:17:09 +0100

    Blocking version of matrix multiplication

--- matmult.c
+++ matmult.c
@@ -1,4 +1,5 @@
 #include <cblas.h>
+#include "math.h"
 #include "matmult.h"
 
 // Basic version of matrix multiplication
@@ -8,7 +9,7 @@ void matmult_nat(int m, int n, int k, double **A, double **B, double **C) {
 
 // Matrix multiplication with the use of CBLAS_DGEMM
 void matmult_lib(int m, int n, int k, double **A, double **B, double **C) {
-    cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,m,n,k,1,*A,k,*B,n,0,*C,n);
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, 1, *A, k, *B, n, 0, *C, n);
 }
 
 // Matrix multiplication in order m->n->k
@@ -18,7 +19,7 @@ void matmult_mnk(int m, int n, int k, double **A, double **B, double **C) {
         for (nn = 0; nn < n; nn++) {
             C[mm][nn] = 0;
             for (kk = 0; kk < k; kk++) {
-                C[mm][nn] += A[mm][kk]*B[kk][nn];
+                C[mm][nn] += A[mm][kk] * B[kk][nn];
             }
         }
     }
@@ -28,10 +29,10 @@ void matmult_mnk(int m, int n, int k, double **A, double **B, double **C) {
 void matmult_mkn(int m, int n, int k, double **A, double **B, double **C) {
     int mm, nn, kk;
     for (mm = 0; mm < m; mm++) {
-        for (kk = 0; kk < k; kk++){
-            for (nn = 0; nn < n; nn++)  {
+        for (kk = 0; kk < k; kk++) {
+            for (nn = 0; nn < n; nn++) {
                 if (kk == 0) C[mm][nn] = 0;
-                C[mm][nn] += A[mm][kk]*B[kk][nn];
+                C[mm][nn] += A[mm][kk] * B[kk][nn];
             }
         }
     }
@@ -42,9 +43,9 @@ void matmult_nmk(int m, int n, int k, double **A, double **B, double **C) {
     int mm, nn, kk;
     for (nn = 0; nn < n; nn++) {
         for (mm = 0; mm < m; mm++) {
-            for (kk = 0; kk < k; kk++)  {
+            for (kk = 0; kk < k; kk++) {
                 if (kk == 0) C[mm][nn] = 0;
-                C[mm][nn] += A[mm][kk]*B[kk][nn];
+                C[mm][nn] += A[mm][kk] * B[kk][nn];
             }
         }
     }
@@ -57,7 +58,7 @@ void matmult_nkm(int m, int n, int k, double **A, double **B, double **C) {
         for (kk = 0; kk < k; kk++) {
             for (mm = 0; mm < m; mm++) {
                 if (kk == 0) C[mm][nn] = 0;
-                C[mm][nn] += A[mm][kk]*B[kk][nn];
+                C[mm][nn] += A[mm][kk] * B[kk][nn];
             }
         }
     }
@@ -66,11 +67,11 @@ void matmult_nkm(int m, int n, int k, double **A, double **B, double **C) {
 // Matrix multiplication in order k->m->n
 void matmult_kmn(int m, int n, int k, double **A, double **B, double **C) {
     int mm, nn, kk;
-    for (kk = 0; kk < k; kk++){
-        for (mm = 0; mm < m; mm++){
-            for (nn = 0; nn < n; nn++)  {
+    for (kk = 0; kk < k; kk++) {
+        for (mm = 0; mm < m; mm++) {
+            for (nn = 0; nn < n; nn++) {
                 if (kk == 0) C[mm][nn] = 0;
-                C[mm][nn] += A[mm][kk]*B[kk][nn];
+                C[mm][nn] += A[mm][kk] * B[kk][nn];
             }
         }
     }
@@ -79,13 +80,38 @@ void matmult_kmn(int m, int n, int k, double **A, double **B, double **C) {
 // Matrix multiplication in order k->n->m
 void matmult_knm(int m, int n, int k, double **A, double **B, double **C) {
     int mm, nn, kk;
-    for (kk = 0; kk < k; kk++){
+    for (kk = 0; kk < k; kk++) {
         for (nn = 0; nn < n; nn++) {
             for (mm = 0; mm < m; mm++) {
                 if (kk == 0) C[mm][nn] = 0;
-                C[mm][nn] += A[mm][kk]*B[kk][nn];
+                C[mm][nn] += A[mm][kk] * B[kk][nn];
             }
         }
     }
 }
 
+// Blocking version of matrix multiplication in order ->m->k->n
+void matmult_blk(int m, int n, int k, double **A, double **B, double **C, int bs) {
+    int i, j;       // indices for filling C with 0
+    int MM, KK, NN; // iterators over blocks
+    int mm, kk, nn; // iterators inside blocks
+
+    for (i = 0; i < m; i++)
+        for (j = 0; j < n; j++)
+            C[mm][nn] = 0.0;
+
+    for (MM = 0; MM < m; MM += bs) {
+        for (KK = 0; KK < k; KK += bs) {
+            for (NN = 0; NN < n; NN += bs) {
+
+                for (kk = KK; kk < fmin(KK + bs, k); kk++) {
+                    for (mm = MM; mm < fmin(MM + bs, m); mm++) {
+                        for (nn = NN; nn < fmin(NN + bs, n); nn++)
+
+                            C[mm][nn] += A[mm][kk] * B[kk][nn];
+                    }
+                }
+            }
+        }
+    }
+}
--- matmult.h
+++ matmult.h
@@ -26,4 +26,13 @@ void matmult_mnk(int m, int n, int k, double **A, double **B, double **C);
 // C - output matrix
 void matmult_lib(int m, int n, int k, double **A, double **B, double **C);
 
+// m - number of rows of matrix A (rows of matrix C)
+// n - number of columns of matrix B (columns of matrix C)
+// k - number of columns of matrix A (rows of matrix B)
+// A - input matrix 1
+// B - input matrix 2
+// C - output matrix
+// bs - block size
+void matmult_blk(int m, int n, int k, double **A, double **B, double **C, int bs);
+
 #endif
