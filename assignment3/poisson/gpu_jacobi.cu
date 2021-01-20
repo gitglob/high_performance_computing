be747844c44ceb918dc8603f00c10f9c441292c7
Paweł <pawel.darulewski@gmail.com>
2021-01-20 16:31:09 +0100

    Fixing function scope

--- assignment3/poisson/gpu_jacobi.cu
+++ assignment3/poisson/gpu_jacobi.cu
@@ -36,23 +36,21 @@ void run_gpu_jacobi_1(double ***u, double ***u_old, double ***f, int N, int delt
 __global__
 void gpu_jacobi_2(double *u, double *u_old, double *f, int N, double *temp_pointer, int delta_2, double div_val) {
 
-        int x = blockIdx.x * blockDim.x + threadIdx.x;
-        int y = blockIdx.y * blockDim.y + threadIdx.y;
-        int z = blockIdx.z * blockDim.z + threadIdx.z;
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int z = blockIdx.z * blockDim.z + threadIdx.z;
 
-        if (x > 1 && x < N - 1 && y > 1 && y < N - 1 && z > 1 && z < N - 1) {
-                u[N * N * x + N * y + z] = (u_old[N * N * (x - 1) + N * y + z] + u_old[N * N * (x + 1) + N * y + z]
-                                          + u_old[N * N * x + N * (y - 1) + z] + u_old[N * N * x + N * (y + 1) + z]
-                                          + u_old[N * N * x + N * y + (z - 1)] + u_old[N * N * x + N * y + (z + 1)]
-                                          + delta_2 * f[N * N * x + N * y + z]) * div_val;
-            }
-
-            temp_pointer = u;
-            u = u_old;
-            u_old = temp_pointer;
-            checkCudaErrors(cudaDeviceSynchronize());
-        }
+    if (x > 1 && x < N - 1 && y > 1 && y < N - 1 && z > 1 && z < N - 1) {
+            u[N * N * x + N * y + z] = (u_old[N * N * (x - 1) + N * y + z] + u_old[N * N * (x + 1) + N * y + z]
+                                      + u_old[N * N * x + N * (y - 1) + z] + u_old[N * N * x + N * (y + 1) + z]
+                                      + u_old[N * N * x + N * y + (z - 1)] + u_old[N * N * x + N * y + (z + 1)]
+                                      + delta_2 * f[N * N * x + N * y + z]) * div_val;
     }
+
+    temp_pointer = u;
+    u = u_old;
+    u_old = temp_pointer;
+    checkCudaErrors(cudaDeviceSynchronize());
 }
 
 void run_gpu_jacobi_2(double *u, double *u_old, double *f, int N, int delta, int iter_max, int *iter, dim3 dim_grid, dim3 dim_block) {
